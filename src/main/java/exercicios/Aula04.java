1d5682b7e7ea32c94c30fa43f5720245f6f4f6a4
deoliverrafa <ytsecretgames38@gmail.com>
2025-05-29 20:57:25 -0300

    total estudanteCursoAndSexo e mediaNotaTodosEstudantesCurso

--- src/main/java/exercicios/Aula04.java
+++ src/main/java/exercicios/Aula04.java
@@ -55,13 +55,15 @@ public class Aula04 extends Aula {
     }
 
     protected long totalEstudantesCursoAndSexo(@NonNull final Stream<Estudante> stream, @NonNull final Curso curso, final char sexo) {
-        // TODO: Você precisa implementar este método. Apague estas linhas e escreva o código correto.
-        return -1;
+        return  stream.filter(e -> e.hasCurso() == true && e.getCurso().equals(curso) && e.getSexo() == sexo)
+                .count();
     }
 
     protected double mediaNotaTodosEstudantesCurso(@NonNull final Stream<Estudante> stream, @NonNull final Curso curso){
-        // TODO: Você precisa implementar este método. Apague estas linhas e escreva o código correto.
-        return -1;
+        return stream.filter(e -> e.hasCurso() == true && e.getCurso().equals(curso))
+                .mapToDouble(e -> e.getNota())
+                .average()
+                .orElse(0);
     }
 
     protected double maiorNotaTodosEstudantes(@NonNull final Stream<Estudante> stream){
