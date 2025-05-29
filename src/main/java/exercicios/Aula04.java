5ccedbe1d96004dbce53804cfa3899bc1a573ddc
deoliverrafa <ytsecretgames38@gmail.com>
2025-05-29 20:55:51 -0300

    criando maiorNOtaCursoAndSexo

--- src/main/java/exercicios/Aula04.java
+++ src/main/java/exercicios/Aula04.java
@@ -48,8 +48,10 @@ public class Aula04 extends Aula {
     }
 
     protected double maiorNotaCursoAndSexo(@NonNull final Stream<Estudante> stream, @NonNull final Curso curso, final char sexo) {
-        // TODO: Você precisa implementar este método. Apague estas linhas e escreva o código correto.
-        return -1;
+        return  stream.filter(e -> e.hasCurso() == true && e.getCurso().equals(curso) && e.getSexo() == sexo)
+                .mapToDouble(e -> e.getNota())
+                .max()
+                .orElse(0);
     }
 
     protected long totalEstudantesCursoAndSexo(@NonNull final Stream<Estudante> stream, @NonNull final Curso curso, final char sexo) {
